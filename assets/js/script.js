58b402b244e31fe7e254c47aa6774906023b184c
peejaywk <peejaywk@gmail.com>
2021-03-02 17:22:20 +0000

    Update home page (league section) with the data read from the API.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Code from https://obfuscator.io/. Used to hide the API_FOOTBALL API Key
-var _0x17d6=['9811CaACaI','1323116hMGSjL','581741oujLdu','460783NcTkWs','34GQsILH','831227XIEAfm','45604CTJBHL','2blzVbM','18351tUSCdr','1LivCDg','359pDzJPO','bac16047d5msh4c42fd455664d80p1c6cdajsn6a053785a5e9','34SEtptG'];var _0x2eec=function(_0x225714,_0x5b7e6e){_0x225714=_0x225714-0xcb;var _0x17d6f0=_0x17d6[_0x225714];return _0x17d6f0;};(function(_0x4ce0d7,_0x5066fb){var _0x27ec8d=_0x2eec;while(!![]){try{var _0x1c256c=-parseInt(_0x27ec8d(0xd7))*parseInt(_0x27ec8d(0xd3))+-parseInt(_0x27ec8d(0xd0))+-parseInt(_0x27ec8d(0xd1))*-parseInt(_0x27ec8d(0xcf))+-parseInt(_0x27ec8d(0xd2))*parseInt(_0x27ec8d(0xce))+parseInt(_0x27ec8d(0xcd))*-parseInt(_0x27ec8d(0xd4))+-parseInt(_0x27ec8d(0xcc))+-parseInt(_0x27ec8d(0xd5))*-parseInt(_0x27ec8d(0xcb));if(_0x1c256c===_0x5066fb)break;else _0x4ce0d7['push'](_0x4ce0d7['shift']());}catch(_0x427680){_0x4ce0d7['push'](_0x4ce0d7['shift']());}}}(_0x17d6,0xc123d));function myAPIKey(){var _0xc06af4=_0x2eec;return _0xc06af4(0xd6);}myAPIKey();
+var _0x17d6 = ['9811CaACaI', '1323116hMGSjL', '581741oujLdu', '460783NcTkWs', '34GQsILH', '831227XIEAfm', '45604CTJBHL', '2blzVbM', '18351tUSCdr', '1LivCDg', '359pDzJPO', 'bac16047d5msh4c42fd455664d80p1c6cdajsn6a053785a5e9', '34SEtptG']; var _0x2eec = function (_0x225714, _0x5b7e6e) { _0x225714 = _0x225714 - 0xcb; var _0x17d6f0 = _0x17d6[_0x225714]; return _0x17d6f0; }; (function (_0x4ce0d7, _0x5066fb) { var _0x27ec8d = _0x2eec; while (!![]) { try { var _0x1c256c = -parseInt(_0x27ec8d(0xd7)) * parseInt(_0x27ec8d(0xd3)) + -parseInt(_0x27ec8d(0xd0)) + -parseInt(_0x27ec8d(0xd1)) * -parseInt(_0x27ec8d(0xcf)) + -parseInt(_0x27ec8d(0xd2)) * parseInt(_0x27ec8d(0xce)) + parseInt(_0x27ec8d(0xcd)) * -parseInt(_0x27ec8d(0xd4)) + -parseInt(_0x27ec8d(0xcc)) + -parseInt(_0x27ec8d(0xd5)) * -parseInt(_0x27ec8d(0xcb)); if (_0x1c256c === _0x5066fb) break; else _0x4ce0d7['push'](_0x4ce0d7['shift']()); } catch (_0x427680) { _0x4ce0d7['push'](_0x4ce0d7['shift']()); } } }(_0x17d6, 0xc123d)); function myAPIKey() { var _0xc06af4 = _0x2eec; return _0xc06af4(0xd6); } myAPIKey();
 
 
 // Example code to read the API-FOOTBALL API copied from https://rapidapi.com/api-sports/api/api-football/endpoints
@@ -16,9 +16,9 @@ function getData(url, cb) {
         }
     };
     //ToDo: Add code to response to different ready states and status messages.
-    
+
     const MY_API = myAPIKey();
- 
+
     xhr.open("GET", "https://api-football-v1.p.rapidapi.com/" + url);
     xhr.setRequestHeader("x-rapidapi-key", MY_API);
     xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
@@ -56,31 +56,39 @@ function getCurrentLeagues(country) {
         data = JSON.parse(localStorage.getItem('leagueData'));
         localStorage.setItem('lastDateLeagueRead', timeNow);
     }
-
     return data;
 }
 
 // Function to search 'data' for a league named 'leagueName'.
-// Returns the league ID if found, otherwise will return null.
+// Returns the league data if found, otherwise will return null.
 function getLeagueID(data, leagueName) {
-    var league_id = null;
+    var leagueData = null;
     $.each(data.api.leagues, function (index, value) {
         if (value.name == leagueName) {
-             league_id = value.league_id;
+            leagueData = value;
         }
     });
-    return league_id;
+    return leagueData;
+}
+
+function updateHomePage(leagueData) {
+    return `
+        <p>${leagueData.name}</p>
+        <img src="${leagueData.logo}" width="80" height="80" alt="League Logo">
+        <p>Season Start: ${leagueData.season_start}</p>
+        <p>Season End: ${leagueData.season_end}</p>
+    `
 }
 
 // Execute the function once the DOM is ready.
 $(document).ready(function () {
     // Get a list of the current active leagues in England
-    var retrievedData = getCurrentLeagues('england');
+    var leagueDataCountry = getCurrentLeagues('england');
 
     // Find the league ID for the Premiership
-    var leagueID = getLeagueID(retrievedData, 'Premier League');
+    var leagueData = getLeagueID(leagueDataCountry, 'Premier League');
+    
+    $("#league-info").html(updateHomePage(leagueData));
 
-    console.log(retrievedData);
-    console.log('League ID: ' + leagueID);
-
+    console.log(leagueDataCountry);
 })
