2a79ba8b087d9e3460101d2e5fc3db5e712ab623
peejaywk <peejaywk@gmail.com>
2021-03-08 09:40:03 +0000

    Move the getData function into a different js file to it can be called from multiple scripts. Need to access this function from different pages. Add function to read the team data from the API and display on the team page.

---
+++ assets/js/apifootball.js
@@ -0,0 +1,40 @@
+// Code from https://obfuscator.io/. Used to hide the API_FOOTBALL API Key
+var _0x17d6 = ['9811CaACaI', '1323116hMGSjL', '581741oujLdu', '460783NcTkWs', '34GQsILH', '831227XIEAfm', '45604CTJBHL', '2blzVbM', '18351tUSCdr', '1LivCDg', '359pDzJPO', 'bac16047d5msh4c42fd455664d80p1c6cdajsn6a053785a5e9', '34SEtptG']; var _0x2eec = function (_0x225714, _0x5b7e6e) { _0x225714 = _0x225714 - 0xcb; var _0x17d6f0 = _0x17d6[_0x225714]; return _0x17d6f0; }; (function (_0x4ce0d7, _0x5066fb) { var _0x27ec8d = _0x2eec; while (!![]) { try { var _0x1c256c = -parseInt(_0x27ec8d(0xd7)) * parseInt(_0x27ec8d(0xd3)) + -parseInt(_0x27ec8d(0xd0)) + -parseInt(_0x27ec8d(0xd1)) * -parseInt(_0x27ec8d(0xcf)) + -parseInt(_0x27ec8d(0xd2)) * parseInt(_0x27ec8d(0xce)) + parseInt(_0x27ec8d(0xcd)) * -parseInt(_0x27ec8d(0xd4)) + -parseInt(_0x27ec8d(0xcc)) + -parseInt(_0x27ec8d(0xd5)) * -parseInt(_0x27ec8d(0xcb)); if (_0x1c256c === _0x5066fb) break; else _0x4ce0d7['push'](_0x4ce0d7['shift']()); } catch (_0x427680) { _0x4ce0d7['push'](_0x4ce0d7['shift']()); } } }(_0x17d6, 0xc123d)); function myAPIKey() { var _0xc06af4 = _0x2eec; return _0xc06af4(0xd6); } myAPIKey();
+
+
+// Example code to read the API-FOOTBALL API copied from https://rapidapi.com/api-sports/api/api-football/endpoints
+// Modified to add a custom URL and to store the data locally to reduce the number of calls to the API.
+// The data will be updated depending on the value of the 'frequency' term passed in.
+async function getData(url, store_id, frequency) {
+    // Get current time
+    var timeNow = new Date().getTime();
+
+    // Retrieve the time that the requested data was last read
+    var timeDataLastRead = 0;
+    if (localStorage.getItem('timeDataLastRead' + store_id) != null) {
+        timeDataLastRead = localStorage.getItem('timeDataLastRead' + store_id);
+    }
+
+    // If the data has expired the read the API - else retrieve from local storage
+    if (timeNow >= parseInt(timeDataLastRead) + parseInt(frequency)) {
+        const MY_API = myAPIKey();
+        console.log('Data is stale so perform an API read.');
+        let response = await fetch("https://api-football-v1.p.rapidapi.com/" + url, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": `${MY_API}`,
+                "x-rapidapi-host": "api-football-v1.p.rapidapi.com"
+            }
+        });
+
+        let data = await response.json();
+        localStorage.setItem('localData' + store_id, JSON.stringify(data));
+        localStorage.setItem('timeDataLastRead' + store_id, timeNow);
+        return data;
+    } else {
+        console.log('Data is valid. No API call required.');
+        let response = await JSON.parse(localStorage.getItem('localData' + store_id));
+        let data = await response;
+        return data;
+    }
+}
--- assets/js/teamdata.js
+++ assets/js/teamdata.js
@@ -1,5 +1,30 @@
-const queryString = window.location.search;
-console.log(queryString);
-const urlParams = new URLSearchParams(queryString);
-const team_id = urlParams.get('team_id')
-console.log(team_id);
+function updateTeamInfo(teamData) {
+    return `
+        <h4>${teamData.api.teams[0].name}</h4>
+        <img src="${teamData.api.teams[0].logo}" min-width="80" min-height="80" alt="League Logo">
+        <p>Founded: ${teamData.api.teams[0].founded}</p>
+        <p>Venue: ${teamData.api.teams[0].venue_name}</p>
+        <p>Venue Address: ${teamData.api.teams[0].venue_address}</p>
+        <p>Venue City: ${teamData.api.teams[0].venue_city}</p>
+        <p>Venue Capacity: ${teamData.api.teams[0].venue_capacity}</p>
+    `
+}
+
+// Execute the function once the DOM is ready.
+$(document).ready(function () {
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const team_id = urlParams.get('team_id')
+    console.log(team_id);
+
+    // Calculate one day in milliseconds = (day * hours * minutes * seconds * msec)
+    var oneDay = 1 * 24 * 60 * 60 * 1000;
+    var url = "v2/teams/team/" + team_id;
+    
+    getData(url, 'team'+team_id, oneDay).then(data => {
+        console.log(data);
+        $("#team-info").html(updateTeamInfo(data));
+    });
+
+});
