f7d9a1199865153aac555d4e5b3bd8e54ccee7d0
peejaywk <peejaywk@gmail.com>
2021-03-30 18:07:49 +0000

    Add spinner to the Send butto on the contact page when form has been submitted. Used to give the user some feedback that something is happening in the background.

--- assets/js/contact-email.js
+++ assets/js/contact-email.js
@@ -1,4 +1,17 @@
+/**
+ * On form submission send information to emailJS. Add spinner to the button whilst waiting for a response from emailJS
+ * On success display the modal, clear the form and remove the spinner.
+ * On error display an alert for the user.
+ * @param {*} contactForm 
+ */
 function sendMail(contactForm) {
+    // CREDIT: Adding a spinner to a button (https://bbbootstrap.com/snippets/loading-spinner-button-click-32423058)
+    // Disable button
+    $("#send-button").prop("disabled", true);
+    // Add spinner to button
+    $("#send-button").html(
+        '<i class="fa fa-circle-o-notch fa-spin"></i> Sending...'
+    );
     // Extract information for the email from the contact form.
     emailjs.send('service_6txdjqc', 'premier-football', {
         "from_name": contactForm.name.value,
@@ -7,15 +20,20 @@ function sendMail(contactForm) {
         "contact-subject": contactForm.subject.value,
         "contact-message": contactForm.message.value
     })
-    .then(
-        function(response) { // If email sent successfully then open modal
-            console.log("SUCCESS", response);
-            $('#success-modal').modal('show');
-            contactForm.reset();
-        },
-        function(error) { // If emailed failed to send then log to console.
-            console.log("FAILED", error);
-        });
-    
+        .then(
+            function (response) { // If email sent successfully then open modal
+                // Re-enable the button and remove spinner
+                $("#send-button").prop("disabled", false);
+                $("#send-button").html('Send');
+                $('#success-modal').modal('show');
+                contactForm.reset();
+            },
+            function (error) { // If emailed failed to send then log to console.
+                alert("Email failed to send. Please contact premierfootball.ci@gmail.com if the problem persists.");
+                // Re-enable the button and remove spinner
+                $("#send-button").prop("disabled", false);
+                $("#send-button").html('Send');
+            });
+
     return false;
 }
